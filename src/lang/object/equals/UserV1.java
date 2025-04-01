79a5b8c594e980a6bb54b1eef5f2c9c05528746a
강경지 <kangkj001@gmail.com>
2025-04-01 17:48:10 +0900

    20250401

---
+++ src/lang/object/equals/UserV1.java
@@ -0,0 +1,15 @@
+package lang.object.equals;
+
+public class UserV1 {
+    public String getId() {
+        return id;
+    }
+
+    private String id;
+
+
+    public UserV1 (String id){
+        this.id = id;
+    }
+
+}
